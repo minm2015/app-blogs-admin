6099749a9b056e6337f03dba9edeb17c6f7c2e0a
minmxls <minmxls@yeah.net>
2018-11-16 19:45:06 +0800

    update menu active

--- src/views/base/menu.vue
+++ src/views/base/menu.vue
@@ -1,19 +1,30 @@
 <template>
     <!-- 菜单 -->
     <ul class="sidebar-nav">
-        <li class="sidebar-nav-link">
-            <a href="javascript:void(0);" class="active">
-                <i class="am-icon-home sidebar-nav-link-logo"></i> 首页
-            </a>
-        </li>
-        <li class="sidebar-nav-link">
-            <a href="javascript:void(0);">
-                <i class="am-icon-eyedropper sidebar-nav-link-logo"></i> 测试
+        <li v-for="(item, index) in menuList" :key="index" class="sidebar-nav-link">
+            <a href="javascript:void(0);" :class="{active: isActive == index}" @click="activeMenu(index)">
+              <i class="sidebar-nav-link-logo" :class="item.class"></i>
+               {{item.name}}
             </a>
         </li>
     </ul>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      isActive: 0,
+      menuList: [
+        {'name': '首页', 'class': 'am-icon-home'},
+        {'name': '测试', 'class': 'am-icon-eyedropper'}
+      ]
+    }
+  },
+  methods: {
+    activeMenu: function (index) {
+      this.isActive = index
+      $('.container-fluid').html('hello word')
+    }
+  }
 }
 </script>
