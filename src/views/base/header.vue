0e3d06d240763b2faa5cd56c9f671d9615043d7c
minmxls@yeah.net <minmlxs@yeah.net>
2018-11-23 01:25:15 +0800

    commit

--- src/views/base/header.vue
+++ src/views/base/header.vue
@@ -1,28 +1,42 @@
 <template>
   <el-header>
-    <div class="topbar-left app-log">
-        <router-link to="/">
+    <div class="topbar-left app-log topbar-head">
+        <router-link to="/" class="topbar-btn">
             测试效能平台
         </router-link>
     </div>
-    <div class="topbar-left">
-        <el-menu mode="horizontal" background-color="#545c64" text-color="#fff"
-        active-text-color="#33cde5" class="app-el-menu-top">
-            <el-menu-item index="1" class="app-el-menu-item-top">首页</el-menu-item>
-            <el-menu-item index="2" class="app-el-menu-item-top">我的</el-menu-item>
-            <el-menu-item index="3" class="app-el-menu-item-top">项目</el-menu-item>
+    <div class="topbar-head topbar-left app-top-nav">
+        <el-menu mode="horizontal" background-color="#373d41" text-color="#fff" active-text-color="#33cde5">
+            <el-menu-item index="1" class="topbar-btn">
+              <router-link to="welcome">首页</router-link>
+            </el-menu-item>
+            <el-menu-item index="2" class="topbar-btn">
+              <router-link to="welcome">我的</router-link>
+            </el-menu-item>
+            <el-menu-item index="3" class="topbar-btn">
+               <router-link to="welcome">项目</router-link>
+            </el-menu-item>
         </el-menu>
     </div>
-    <div class="topbar-right app-language">
-        <el-dropdown size="small" @command="tglanguage">
-            <span class="el-dropdown-link">
-                {{language.value}}<i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item command="en">English</el-dropdown-item>
-                <el-dropdown-item command="cn">简体中文</el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
+    <div class="topbar-right">
+      <div class="topbar-head topbar-left app-top-search">
+        <span class="topbar-btn"><li class="el-icon-search"></li></span>
+      </div>
+      <div class="topbar-head topbar-left app-top-add">
+        <span class="topbar-btn"><li class="el-icon-plus"></li></span>
+      </div>
+      <div class="topbar-head topbar-left app-top-setting">
+        <span class="topbar-btn"><li class="el-icon-setting"></li></span>
+      </div>
+      <div class="topbar-right app-language">
+          <el-dropdown  @command="tglanguage"  trigger="click" style="width:80px" class="topbar-btn">
+              <span class="el-dropdown-link">{{language.value}}<i class="el-icon-arrow-down el-icon--right" style="float: right; line-height: 50px;"></i></span>
+              <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="en">English</el-dropdown-item>
+                  <el-dropdown-item command="cn">简体中文</el-dropdown-item>
+              </el-dropdown-menu>
+          </el-dropdown>
+      </div>
     </div>
   </el-header>
 </template>
@@ -52,7 +66,6 @@ export default {
         this.language.value = '简体中文'
       }
       localStorage.setItem(this.language.name, JSON.stringify(this.language))
-      this.$router.go(0)
     }
   }
 }
