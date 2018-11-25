62add48355d46fee015ba1e5dd116b38b70ef2c9
minmxls@yeah.net <minmlxs@yeah.net>
2018-11-26 00:22:27 +0800

    1111

--- src/views/base/header.vue
+++ src/views/base/header.vue
@@ -1,6 +1,6 @@
 <template>
   <el-header class="top-container">
-    <div class="topbar-left">
+    <div class="topbar-left app-log">
         <router-link to="/" class="topbar-btn">
             测试效能平台
         </router-link>
@@ -12,45 +12,46 @@
               <router-link to="welcome" class="topbar-btn">首页</router-link>
             </el-menu-item>
             <el-menu-item index="2">
-              <router-link to="my" class="topbar-btn">我的</router-link>
+               <router-link to="welcome" class="topbar-btn">项目</router-link>
             </el-menu-item>
             <el-menu-item index="3">
-               <router-link to="welcome" class="topbar-btn">项目</router-link>
+              <router-link to="my" class="topbar-btn">我的</router-link>
+            </el-menu-item>
+            <el-menu-item index="4">
+               <router-link to="welcome" class="topbar-btn">服务</router-link>
             </el-menu-item>
         </el-menu>
     </div>
     <div class="topbar-right top-right-container">
       <div class="topbar-left">
-        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-search"></li></span>
+        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-search"></li>搜索</span>
       </div>
       <div class="topbar-left topbar-right-dropdown">
-        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-plus"></li></span>
-        <ul class="topbar-right-menu-list">
-          <li><router-link to="">add</router-link></li>
-          <li>bbbb</li>
-          <li>cccc</li>
-          <li>dddd</li>
+        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-plus"></li>添加</span>
+        <ul class="topbar-right-menu-list-add">
+          <li><router-link to="">新增任务</router-link></li>
+          <li><router-link to="">新增缺陷</router-link></li>
+          <li><router-link to="">新增需求</router-link></li>
+          <li><span style="border-bottom: 1px solid #ddd;"></span></li>
+          <li><router-link to="">新增项目</router-link></li>
+          <li><router-link to="">新增项目集</router-link></li>
         </ul>
       </div>
       <div class="topbar-left topbar-right-dropdown">
-        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-bell"></li></span>
+        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-bell"></li>消息</span>
       </div>
       <div class="topbar-left topbar-right-dropdown">
-        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-setting"></li></span>
+        <span class="topbar-btn top-right-topbar-btn"><li class="el-icon-setting"></li>配置</span>
       </div>
       <div class="topbar-left">
         <span class="topbar-btn top-right-topbar-btn app-top-user">谢*临时</span>
       </div>
       <div class="topbar-left">
-        <span class="topbar-btn top-right-topbar-btn">
-          <el-dropdown  @command="tglanguage"  trigger="click" style="width:80px">
-              <span class="el-dropdown-link">{{language.value}}<i class="el-icon-arrow-down el-icon--right" style="float: right; line-height: 50px;"></i></span>
-              <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item command="en">English</el-dropdown-item>
-                  <el-dropdown-item command="cn">简体中文</el-dropdown-item>
-              </el-dropdown-menu>
-          </el-dropdown>
-        </span>
+        <span class="topbar-btn top-right-topbar-btn">{{language.value}}</span>
+        <ul class="topbar-right-menu-list-add">
+          <li @click="tglanguage('cn')"><router-link to="">简体中文</router-link></li>
+          <li @click="tglanguage('en')"><router-link to="">English</router-link></li>
+        </ul>
       </div>
     </div>
   </el-header>
